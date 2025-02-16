955be8270a157e71d09cfaea974f44e737ceeab3
k0789789 <k0789789@naver.com>
2025-02-16 22:22:34 +0900

    Fix : 주석 변경

--- Q3-2.sql
+++ Q3-2.sql
@@ -1,5 +1,5 @@
 SELECT *
 FROM approvals
-WHERE approver_id = {USER_ID} -- 특정 사용자가 승인자로 지정된 결재건
+WHERE approver_id = ${USER_ID} -- 특정 사용자가 승인자로 지정된 결재건
 AND status = 'PENDING' -- 아직 처리되지 않은 결재건
-ORDER BY created_at ASC; -- 오래된 요청 순서대로 정렬렬
+ORDER BY created_at ASC; -- 오래된 요청 순서대로 정렬
