dd2b10d4e4b84f355bab78d63ba26ab0b71c50cd
k0789789 <k0789789@naver.com>
2025-02-16 19:19:16 +0900

    Fix : 컬럼명 수정

--- Q3-2.sql
+++ Q3-2.sql
@@ -1,5 +1,5 @@
 SELECT *
-FROM approval_request
+FROM approvals
 WHERE approver_id = {USER_ID} -- 특정 사용자가 승인자로 지정된 결재건
-AND approval_status = 'PENDING' -- 아직 처리되지 않은 결재건
-ORDER BY requested_at ASC; -- 요청된 순서대로 정렬
+AND status = 'PENDING' -- 아직 처리되지 않은 결재건
+ORDER BY created_at ASC; -- 오래된 요청 순서대로 정렬렬
