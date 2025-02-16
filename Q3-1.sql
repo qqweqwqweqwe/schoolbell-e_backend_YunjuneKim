1b82186eea31ec4544caff65e051cd5e99c822c8
k0789789 <k0789789@naver.com>
2025-02-16 19:17:45 +0900

    Feat : 설명 추가

--- Q3-1.sql
+++ Q3-1.sql
@@ -1,14 +1,16 @@
 CREATE TABLE approvals (
     id                 BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 결재 ID
-    requestor_id       BIGINT UNSIGNED NOT NULL, -- 결재를 요청한 사용자 ID
-    approver_id        BIGINT UNSIGNED NOT NULL, -- 결재를 요청받은 사용자 ID
+    requestor_id       BIGINT UNSIGNED NOT NULL, -- 결재를 요청한 사용자 ID (FK)
+    approver_id        BIGINT UNSIGNED NOT NULL, -- 결재를 요청받은 사용자 ID (FK)
     title              VARCHAR(255) NOT NULL, -- 결재 제목
-    content            TEXT NOT NULL, -- 결재 내용
+    content            TEXT NOT NULL, -- 결재 요청 내용
     status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING', -- 결재 상태
-    created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 결재 생성 시간간
-    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 상태가 변경된 시간 (반려 or 승인인)
+    created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 결재 생성 시간
+    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 및 승인으로 인한 상태 변경 or 결재 제목 및 내용 변경 or 결재자 변경 시간 추적
 
+    -- 외래 키 설정: users 테이블의 id를 참조
     CONSTRAINT fk_approval_requestor FOREIGN KEY (requestor_id) REFERENCES users(id)
+    -- 외래 키 설정: users 테이블의 id를 참조
     CONSTRAINT fk_approval_approver FOREIGN KEY (approver_id) REFERENCES users(id)
 )
 
@@ -17,22 +19,22 @@ CREATE TABLE rejected_approvals (
     approval_id        BIGINT UNSIGNED NOT NULL, -- 반려된 결재 요청 ID (FK)
     reason             VARCHAR(255) NOT NULL, -- 반려 사유
     created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 반려 시간
-    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 만약에 반려사유가 변경되었을 시 update
+    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 사유가 추후에 변경될 수도 있으므로 추가가
 
+    -- 외래 키 설정: approvals 테이블의 id를 참조
+    -- 해당 결재 내용이 삭제되면 반려 결재 내용도 삭제되게끔 cascade 적용
     CONSTRAINT fk_rejection_approval FOREIGN KEY (approval_id) REFERENCES approvals(id) ON DELETE CASCADE,
 )
 
 CREATE TABLE approval_logs (
     id                 BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 결재 로그 ID
     approval_id        BIGINT UNSIGNED NOT NULL, -- 결재 요청 ID (FK)
-    action_type        ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL, -- 결재 처리 유형
-    action_by          BIGINT UNSIGNED NOT NULL, -- 처리한 사용자 ID
+    approvel_id        BIGINT UNSIGNED NOT NULL, -- 결재를 처리한 사용자 ID (FK)
+    status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL, -- 결재 처리 유형
     created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 처리 시간
 
     -- 외래 키 설정: approval 테이블의 id를 참조
     CONSTRAINT fk_logs_approval FOREIGN KEY (approval_id) REFERENCES approval(id) ON DELETE CASCADE,
-    
     -- 외래 키 설정: 처리한 사용자 ID (users 테이블의 id를 참조)
     CONSTRAINT fk_logs_action_by FOREIGN KEY (action_by) REFERENCES users(id) ON DELETE RESTRICT,
-
-) 
+)
