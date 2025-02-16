f7be0f2e2549b4e01e6c28a5d7d6398d8f372782
k0789789 <k0789789@naver.com>
2025-02-16 21:38:35 +0900

    Fix : 주석 변경

--- Q3-1.sql
+++ Q3-1.sql
@@ -1,17 +1,17 @@
 CREATE TABLE approvals (
     id                 BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 결재 ID
     requestor_id       BIGINT UNSIGNED NOT NULL, -- 결재를 요청한 사용자 ID (FK)
-    approver_id        BIGINT UNSIGNED NOT NULL, -- 결재를 요청받은 사용자 ID (FK)
+    approver_id        BIGINT UNSIGNED, -- 결재를 요청받은 사용자 ID (FK)
     title              VARCHAR(255) NOT NULL, -- 결재 제목
-    content            TEXT NOT NULL, -- 결재 요청 내용
+    contents           TEXT NOT NULL, -- 결재 요청 내용
     status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING', -- 결재 상태
-    created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 결재 생성 시간
-    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 및 승인으로 인한 상태 변경 or 결재 제목 및 내용 변경 or 결재자 변경
+    created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 생성 시간
+    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, --  변경 시간
 
-    -- 외래 키 설정: users 테이블의 id를 참조
-    CONSTRAINT fk_approval_requestor FOREIGN KEY (requestor_id) REFERENCES users(id)
-    -- 외래 키 설정: users 테이블의 id를 참조
-    CONSTRAINT fk_approval_approver FOREIGN KEY (approver_id) REFERENCES users(id)
+    -- 외래 키 설정: 결재를 요청한 사용자의 ID (users 테이블의 id 참조)
+    CONSTRAINT fk_approval_requestor FOREIGN KEY (requestor_id) REFERENCES users(id) ON DELETE CASCADE,
+    -- 외래 키 설정: 결재를 요청받은 사용자의 ID (users 테이블의 id 참조)
+    CONSTRAINT fk_approval_approver FOREIGN KEY (approver_id) REFERENCES users(id) ON DELETE CASCADE,
 )
 
 CREATE TABLE rejected_approvals (
@@ -19,7 +19,7 @@ CREATE TABLE rejected_approvals (
     approval_id        BIGINT UNSIGNED NOT NULL, -- 반려된 결재 요청 ID (FK)
     reason             VARCHAR(255) NOT NULL, -- 반려 사유
     created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 반려 시간
-    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 사유가 추후에 변경될 수도 있으므로 추가가
+    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 사유가 추후에 변경될 수도 있으므로 추가
 
     -- 외래 키 설정: approvals 테이블의 id를 참조
     -- 해당 결재 내용이 삭제되면 반려 결재 내용도 삭제되게끔 cascade 적용
