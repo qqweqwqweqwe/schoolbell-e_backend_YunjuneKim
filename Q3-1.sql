b2e9af3e5ada3fa1b852a6a2ae1c89dee14edda0
k0789789 <k0789789@naver.com>
2025-02-16 19:49:15 +0900

    Fix : 결재 로그 테이블 변경

--- Q3-1.sql
+++ Q3-1.sql
@@ -6,7 +6,7 @@ CREATE TABLE approvals (
     content            TEXT NOT NULL, -- 결재 요청 내용
     status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING', -- 결재 상태
     created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 결재 생성 시간
-    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 및 승인으로 인한 상태 변경 or 결재 제목 및 내용 변경 or 결재자 변경 시간 추적
+    updated_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 반려 및 승인으로 인한 상태 변경 or 결재 제목 및 내용 변경 or 결재자 변경
 
     -- 외래 키 설정: users 테이블의 id를 참조
     CONSTRAINT fk_approval_requestor FOREIGN KEY (requestor_id) REFERENCES users(id)
@@ -26,11 +26,24 @@ CREATE TABLE rejected_approvals (
     CONSTRAINT fk_rejection_approval FOREIGN KEY (approval_id) REFERENCES approvals(id) ON DELETE CASCADE,
 )
 
+/**
+결재 로그
+결재 제목 변경 추적
+결재 내용 변경 추적
+결재 상태 변경 추적
+결재자 변경 상태 추적
+**/
 CREATE TABLE approval_logs (
     id                 BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 결재 로그 ID
     approval_id        BIGINT UNSIGNED NOT NULL, -- 결재 요청 ID (FK)
-    approvel_id        BIGINT UNSIGNED NOT NULL, -- 결재를 처리한 사용자 ID (FK)
-    status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL, -- 결재 처리 유형
+    old_approvel_id        BIGINT UNSIGNED NOT NULL, -- 결재를 처리한 사용자 ID (FK)
+    new_approvel_id        BIGINT UNSIGNED NOT NULL, -- 결재를 처리한 사용자 ID (FK)
+    old_status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL, -- 결재 처리 유형
+    new_status             ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL, -- 결재 처리 유형
+    old_title              VARCHAR(255) NOT NULL, -- 결재 제목
+    new_title              VARCHAR(255) NOT NULL, -- 결재 제목
+    old_content            TEXT NOT NULL, -- 결재 요청 내용
+    new_content            TEXT NOT NULL, -- 결재 요청 내용
     created_at         DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- 처리 시간
 
     -- 외래 키 설정: approval 테이블의 id를 참조
